1d1a12d8ced119ccfb346ae27b1c50f5080c61ae
JorgeSRS <jorgesr9878@gmail.com>
2022-12-07 22:57:29 -0600

    Create pdf.blade.php

---
+++ car-care/resources/views/pdf.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+
+<table class="table table-hover table-striped">
+    <thead>
+        <tr>
+            <th>foto</th>
+            <th>marca</th>
+            <th>modelo</th>
+            <th>daño</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($autos as $auto)
+        <tr>
+            <td>
+            <img style="height: 100px; width: 100px;
+            background-color: #EFEFEF; margin: 20px;
+            " class="card-img-top rounded-circle mx-auto d-block" 
+            src="images/{{$auto->foto}}""> 
+            </td>
+            <td>{{$auto->marca}}</td>
+            <td>{{$auto->modelo}}</td>
+            <td>{{$auto->daño}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
